9c158d804939fe056b6983b914d73ec6add05170
iamProud <proud.berlin@gmail.com>
2022-11-10 15:44:24 +0100

    add own problem robo_sokoban

--- gym_pcgrl/envs/probs/__init__.py
+++ gym_pcgrl/envs/probs/__init__.py
@@ -1,9 +1,11 @@
+import gym_pcgrl.envs
 from gym_pcgrl.envs.probs.binary_prob import BinaryProblem
 from gym_pcgrl.envs.probs.ddave_prob import DDaveProblem
 from gym_pcgrl.envs.probs.mdungeon_prob import MDungeonProblem
 from gym_pcgrl.envs.probs.sokoban_prob import SokobanProblem
 from gym_pcgrl.envs.probs.zelda_prob import ZeldaProblem
 from gym_pcgrl.envs.probs.smb_prob import SMBProblem
+from gym_pcgrl.envs.probs.robo_sokoban_prob import RoboSokobanProblem
 
 # all the problems should be defined here with its corresponding class
 PROBLEMS = {
@@ -12,5 +14,6 @@ PROBLEMS = {
     "mdungeon": MDungeonProblem,
     "sokoban": SokobanProblem,
     "zelda": ZeldaProblem,
-    "smb": SMBProblem
+    "smb": SMBProblem,
+    "robosoko": RoboSokobanProblem
 }
