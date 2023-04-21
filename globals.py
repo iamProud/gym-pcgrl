609fe6aa571cc4696e73946d408b67ceef404416
iamProud <proud.berlin@gmail.com>
2023-04-21 12:31:22 +0200

    rm config file

--- globals.py
+++ globals.py
@@ -1,11 +1,11 @@
 import json
 
-game = 'sokoban'
+game = 'sokoban_tlcls'
 representation = 'turtle'
 run_idx = 2
 render = False
 is_inference = False
-device='cuda:5'
+device='auto'
 
 config = dict(
     width = 8,
@@ -14,17 +14,29 @@ config = dict(
     solver_power = 20000
 )
 
-game_path = f'shared_runs/{config["width"]}x{config["height"]}/{game}'
-run_path = f'{game_path}/{game}_{representation}_{run_idx}_1_log/'
+game_path = f'shared_runs/{config["width"]}x{config["height"]}/sokoban'
+run_path = f'{game_path}/sokoban_{representation}_{run_idx}_2_log/'
 
 config['cropped_size'] = 2 * max(config['width'], config['height'])
 
 print(f'Running {game} with {representation} representation - run {run_idx}')
 
-with open(game_path+"/config.json", "r") as f:
-    config_file = json.load(f)
-    run_config = config_file['run'][str(run_idx)]
+config['max_crates'] = 2
+config['target_solution'] = 15
+config['probabilities'] = {
+    "empty": 0.6,
+    "solid": 0.34,
+    "player": 0.02,
+    "crate": 0.02,
+    "target": 0.02
+}
 
-    for key, value in run_config.items():
-        config[key] = config_file[key][value]
-        print(f'### {key} = {config[key]}')
+
+
+# with open(game_path+"/config.json", "r") as f:
+#     config_file = json.load(f)
+#     run_config = config_file['run'][str(run_idx)]
+#
+#     for key, value in run_config.items():
+#         config[key] = config_file[key][value]
+#         print(f'### {key} = {config[key]}')
